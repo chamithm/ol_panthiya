0ad5cd1fd19afa3486cd38e3e93c36ef7afa7767
chamithm <chamithtech80@gmail.com>
2022-07-22 14:43:35 -0700

    check for create branch

--- lib/video/video_lessions.dart
+++ lib/video/video_lessions.dart
@@ -113,7 +113,7 @@ class TopContainerPortrait extends StatefulWidget {
 
 class _TopContainerPortraitState extends State<TopContainerPortrait> {
   List<Color> containerColor = [Colors.green[200],Colors.white,Colors.white,Colors.white,Colors.white,Colors.white];
-  //edit video_lessons page
+  //check new branch
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
